b39146364c95831fb7cfb5a27b2a6033ce740cda
vx <vx@vxs-MacBook-Pro.local>
2023-09-26 12:52:58 -0600

    git commit "first one"

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: true,
+  useTabs: false,
+  semi: false 
+};
