082c0ce4dcba8cbbced2d789b3afc8d699ce9793
vx <vineethxavier89@gmail.com>
2023-10-11 15:45:50 -0600

    used yarn 4 vector-icons, fixed icon issue, used loadFoant() to fix icon question mark issue. link in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Feather from 'react-native-vector-icons/Feather'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+//https://levelup.gitconnected.com/integrating-react-native-vector-icon-with-react-native-0-60-and-above-4cad9b9c61c5
+Ionicons.loadFont()
+Feather.loadFont()
+FontAwesome5.getStyledIconSet('brand').loadFont()
+FontAwesome5.getStyledIconSet('light').loadFont()
+FontAwesome5.getStyledIconSet('regular').loadFont()
+FontAwesome5.getStyledIconSet('solid').loadFont()
 
 const App = () => {
   return (
@@ -7,7 +17,9 @@ const App = () => {
       <View style={styles.container}>
         <Text style={styles.temp}> 6</Text>
         <Text style={styles.feels}> Feelts like 5</Text>
-
+        <Ionicons name="alarm" size={50} color="#498" />
+        <FontAwesome5 name="amazon" size={40} />
+        <Feather name="sun" size={50} />
         <View style={styles.highLowWrapper}>
           <Text style={styles.highLow}>High: 8 </Text>
           <Text style={styles.highLow}>Low : 6</Text>
